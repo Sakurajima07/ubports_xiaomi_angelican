fcf956c3fd5f8419b37e0b8d494e1ad454c93112
The Great Wizard Azkali <a.ffcc7@gmail.com>
2026-01-08 17:33:06 +0000

    dtbo: add dtbo_ids

--- make-dtboimage.sh
+++ make-dtboimage.sh
@@ -15,12 +15,24 @@ case "$kernel_arch" in
     *) ARCH="$kernel_arch" ;;
 esac
 
-if [ -n "$deviceinfo_dtbo" ]; then
-    PREFIX=$KERNEL_OBJ/arch/$ARCH/boot/dts/
-    DTBO_LIST="$PREFIX${deviceinfo_dtbo// / $PREFIX}"
-else
+if [ -z "$deviceinfo_dtbo" ]; then
     echo "Please define deviceinfo_dtbo in deviceinfo"
     exit 1
 fi
 
-python2 "$TMPDOWN/libufdt/utils/src/mkdtboimg.py" create "$OUT" $DTBO_LIST
+PREFIX=$KERNEL_OBJ/arch/$ARCH/boot/dts/
+
+IFS=" " read -r -a DTBOS <<< "$deviceinfo_dtbo"
+IFS=" " read -r -a IDS <<< "$deviceinfo_dtbo_ids"
+DTBO_ARGS=""
+
+i=0
+for dtbo in "${DTBOS[@]}"; do
+    if [ -n "${IDS[i]}" ]; then
+        DTBO_ARGS="$DTBO_ARGS --id=${IDS[i]}"
+    fi
+    DTBO_ARGS="$DTBO_ARGS $PREFIX${dtbo// / $PREFIX}"
+    i=$((i + 1))
+done
+
+python2 "$TMPDOWN/libufdt/utils/src/mkdtboimg.py" create "$OUT" $DTBO_ARGS
